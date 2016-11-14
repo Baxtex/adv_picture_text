2fd5dcf287ae607d72c9cb98a905f243cfec5416
Baxtex <baxtex@gmail.com>
2016-11-14 17:25:32 +0100

    now working

--- src/assignment2/MorphImage.java
+++ src/assignment2/MorphImage.java
@@ -4,6 +4,7 @@ import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import javax.imageio.ImageIO;
 
@@ -14,32 +15,61 @@ import javax.imageio.ImageIO;
  *
  */
 public class MorphImage {
-	private BufferedImage image;
-
+	private final String URL = "pic1.jpg";
 
 	public static void main(String[] args) {
-		String URL = "/pic2.png";
 		try {
-			new MorphImage(URL);
+			new MorphImage();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public MorphImage(String URL) throws IOException {
-		System.out.println("Morphing image.");
-		BufferedImage img = ImageIO.read(new File(URL));
+	public MorphImage() throws IOException {
+		BufferedImage img = readImage(URL);
+		pointOp(img, 50, 75);
+	}
+
+	/**
+	 * Read and create a bufferedImage from an URL.
+	 * 
+	 * @param URL
+	 * @return
+	 */
+	private BufferedImage readImage(String URL) {
+		URL url = getClass().getResource(URL);
+		File file = new File(url.getPath());
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return img;
+
+	}
+
+	/**
+	 * Uses a BufferedImage to create a new copy of it with altered values.
+	 * 
+	 * @param img
+	 * @throws IOException
+	 */
+	private void pointOp(BufferedImage img, int con, int brig) throws IOException {
 		int width = img.getWidth();
 		int height = img.getHeight();
-		this.image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 		WritableRaster inraster = img.getRaster();
-		WritableRaster outraster = this.image.getRaster();
+		WritableRaster outraster = image.getRaster();
 		for (int i = 0; i < width; i++)
 			for (int j = 0; j < height; j++) {
 				int value = inraster.getSample(i, j, 0);
 				outraster.setSample(i, j, 0, 255 - value);
 			}
-		ImageIO.write(image, "PNG", new File("Morphed" + URL + ".png"));
+		ImageIO.write(image, "PNG", new File("Morphed " + URL));
+		System.out.println("Morphed");
+
 	}
+
 
 }
